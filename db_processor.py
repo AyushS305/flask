41762ad0e0aab5b3deaf42c095eec150b0355aea
AyushSrvtv <ayushsrvtv@gmail.com>
2023-06-26 03:30:01 +0530

    added the sql query to update the item_id in sales table

--- db_processor.py
+++ db_processor.py
@@ -11,5 +11,6 @@ def db_injector(dict_with_invoice_data):
             render=tuple([dict_with_invoice_data['Roll No.'],'"'+dict_with_invoice_data['Name']+'"',dict_with_invoice_data['Class'],'"'+dict_with_invoice_data['House']+'"','"'+x+'"',dict_with_invoice_data[x][0],dict_with_invoice_data[x][2],False,'"'+dict_with_invoice_data['Date']+'"', '"'+bill_no+'"'])
             sql1='insert into sales(roll_no,student_name,class,house,item_purchased,item_quantity,total_price,tc_leave,date_of_purchase,bill_no) values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
             cur.execute(sql1%render)
+            cur.execute("UPDATE sales set item_id=(select id from products where item_purchased=item_name)")
             con.commit()
     return bill_no
