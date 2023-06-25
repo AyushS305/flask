56e76ee7a62549bf81b860e5fac1b97369380f08
AyushSrvtv <ayushsrvtv@gmail.com>
2023-06-25 19:53:08 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,57 @@
+from flask import Flask, render_template, request
+from numtoword import number_to_word
+from db_processor import *
+import sqlite3 as sql
+
+app = Flask(__name__)
+
+def db_connect():
+    con = sql.connect('prikaway.db')
+    cur = con.cursor()
+    return cur
+
+@app.route('/')
+def student():
+   cur=db_connect()
+   cur.execute("select item_name from products")
+   y=z=[]
+   for rows in cur.fetchall():
+      y.append(rows[0])  
+   return render_template('student_invoice_input_template.html', items=y)
+
+@app.route('/output',methods = ['POST', 'GET'])
+def output():
+   if request.method == 'POST':
+      out = request.form.to_dict()
+      output=input_template_process(out) 
+      db_injector(output)
+      return render_template("student_invoice_output_template.html",output = output)
+   
+@app.route('/print_invoice')
+def print_invoice():
+   cur=db_connect()
+   cur.execute("select item_name from products")
+
+def input_template_process(out):
+    cur=db_connect()
+    cur.execute("select * from products")
+    rows=cur.fetchall()
+    ter={}
+    s=0
+    for x in out.keys():
+       if x not in ['Name','Class','Roll No.', 'Date', 'House']:
+          if out[x]=='':
+             continue
+          for y in rows:
+             if x==y[1]:
+                ter[x]=([int(out[x]),y[2],int(out[x])*y[2]])
+                s+=int(out[x])*y[2]
+       else:
+          ter[x]=out[x]
+    ter['Grand Total']=s
+    ter['Word Amount']=number_to_word(s)
+    return(ter)
+
+
+if __name__ == '__main__':
+   app.run(debug = True)
---
+++ db_processor.py
@@ -0,0 +1,14 @@
+import sqlite3 as sql
+from datetime import datetime
+
+def db_injector(dict_with_invoice_data):
+    con = sql.connect('prikaway.db')
+    cur = con.cursor()
+    my_date= datetime.strptime(dict_with_invoice_data['Date'], '%Y-%m-%d')
+    bill_no= 'PWPL/RW/'+str(my_date.year)+'/'+str(my_date.month)+'/'+str(dict_with_invoice_data['Roll No.'])
+    for x in dict_with_invoice_data.keys():
+        if x not in ['Roll No.','Name','Class','House','Date', 'Grand Total', 'Word Amount']:
+            render=tuple([dict_with_invoice_data['Roll No.'],'"'+dict_with_invoice_data['Name']+'"',dict_with_invoice_data['Class'],'"'+dict_with_invoice_data['House']+'"','"'+x+'"',dict_with_invoice_data[x][0],dict_with_invoice_data[x][2],False,'"'+dict_with_invoice_data['Date']+'"', '"'+bill_no+'"'])
+            sql1='insert into sales(roll_no,student_name,class,house,item_purchased,item_quantity,total_price,tc_leave,date_of_purchase,bill_no) values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
+            cur.execute(sql1%render)
+            con.commit()
