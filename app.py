2ac9a975d04d2d465aac94977b1cb6e8b0a08f09
AyushSrvtv <ayushsrvtv@gmail.com>
2023-07-04 20:30:57 +0530

    added the session feature

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mail import Mail, Message
 from db_processor import *
 from date_format_change import *
 from process_format import *
+from flask_session import Session
 
 app = Flask(__name__)
 mail = Mail(app)
-
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+app.secret_key = 'BAD_SECRET_KEY'
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = 'cc.prikaway@gmail.com'
@@ -20,9 +24,9 @@ def auth():
    error=None
    if request.method == 'POST':
       result=request.form.to_dict()
-      global sync_user
       result=db_auth(result)
-      sync_user=result
+      for x in result:
+         session[x]=result[x]
       if result['flag'] == False:
          error = 'Invalid Credentials. Please try again.'
       else:
@@ -32,15 +36,15 @@ def auth():
 @app.route('/homepage')
 def homepage():
    
-   return render_template('homepage.html', sync_user=sync_user)
+   return render_template('homepage.html', sync_user=session)
    
 @app.route('/input', methods = ['POST', 'GET'])
 def input ():
    h=[]
    y=[]
-   for rows in db_house_search(sync_user['school_id']):
+   for rows in db_house_search(session['school_id']):
       h.append(rows[0])
-   for rows in db_product_search(sync_user['school_id']):
+   for rows in db_product_search(session['school_id']):
       y.append(rows[0])  
    return render_template('student_invoice_input_template.html', items=y, house=h)
 
@@ -49,16 +53,16 @@ def output():
    if request.method == 'POST':
       out = request.form.to_dict()
       global sync
-      output=input_template_process(out, sync_user['school_id'])
+      output=input_template_process(out, session['school_id'])
       sync=output
-      print(sync_user['img_url']+'"')
-      return render_template("student_invoice_output_template.html",output = output, image=sync_user['img_url'])
+      print(session['img_url']+'"')
+      return render_template("student_invoice_output_template.html",output = output, image=session['img_url'])
    
 @app.route('/print_invoice',methods = ['POST', 'GET'])
 def print_invoice():
    if request.method == 'POST':
       output=sync
-      output['Invoice No.']= db_injector(output, sync_user)
+      output['Invoice No.']= db_injector(output, session)
       output['Date']=change_date_format(output['Date'])
       msg = Message(
                 "STUDENT INVOICE GENERATED# "+output['Invoice No.'],
@@ -68,9 +72,9 @@ def print_invoice():
       
       msg.body = " Please see the details below."
       output=output_template_format(output)
-      msg.html = render_template("student_invoice_print_template.html", output=output, image=sync_user['img_url'])
+      msg.html = render_template("student_invoice_print_template.html", output=output, image=session['img_url'])
       mail.send(msg)
-      return render_template("student_invoice_print_template.html",output = output, image=sync_user['img_url'])
+      return render_template("student_invoice_print_template.html",output = output, image=session['img_url'])
    
 @app.route('/principal_bill',methods=['POST','GET'])
 def principal_bill():
@@ -80,30 +84,30 @@ def principal_bill():
 def generate_bill():
    if request.method == 'POST':
       out = request.form.to_dict()
-      res=db_search(out, sync_user['school_id'])
-      result=school_pricipal_bill_process(res, sync_user['school_code'])
+      res=db_search(out, session['school_id'])
+      result=school_pricipal_bill_process(res, session['school_code'])
       global sync_school_bill
       sync_school_bill=result
-      return render_template('principal_bill_output_template.html', result=result, image=sync_user)
+      return render_template('principal_bill_output_template.html', result=result, image=session)
 
 @app.route('/print_school_bill',methods=['POST','GET'])
 def print_school_bill():
    if request.method == 'POST':
       result=sync_school_bill
       msg = Message(
-                "BILL TO " +(sync_user['school_name']).upper()+ " PRINCIPAL GENERATED# "+result['Invoice No.'],
+                "BILL TO " +(session['school_name']).upper()+ " PRINCIPAL GENERATED# "+result['Invoice No.'],
                 sender ='MailBot',
                 recipients = ['prikawayinvoicemailbot@gmail.com']
                )
       msg.body = "Please see the details below."
-      msg.html = render_template("pricipal_bill_print_template.html", result=result, image=sync_user)
+      msg.html = render_template("pricipal_bill_print_template.html", result=result, image=session)
       mail.send(msg)
-      return render_template("pricipal_bill_print_template.html",result = result, image=sync_user)
+      return render_template("pricipal_bill_print_template.html",result = result, image=session)
    
 @app.route('/cover_page_input', methods=['POST','GET'])
 def cover_page_input():
    h=[]
-   for rows in db_house_search(sync_user['school_id']):
+   for rows in db_house_search(session['school_id']):
       h.append(rows[0])
    return render_template('cover_page_input.html', house=h)
 
@@ -112,11 +116,11 @@ def confirm_cover_page():
    if request.method == 'POST':
          data = request.form.to_dict()
          if data['House'] == 'All':
-            result=db_search_all_house_cover(data, sync_user['school_id'])
+            result=db_search_all_house_cover(data, session['school_id'])
             result=all_house_cover_process(result)
             result['House']='All'
          else:
-            result=db_search_house_cover(data, sync_user['school_id'])
+            result=db_search_house_cover(data, session['school_id'])
             result=house_cover_process(result)
             result['House']=data['House']
          global sync_cover_data
